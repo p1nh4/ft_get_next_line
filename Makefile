0ac96f5791870aa46867588c08944443e0e6fbec
p1nh4 <davidos-@student.42porto.com>
2026-01-11 12:55:40 +0000

    feat: estrutura inicial e Makefile

---
+++ Makefile
@@ -0,0 +1,33 @@
+NAME 		:= get_next_line.a
+CC 	 		:=	cc
+C_FLAGS		:= -Wall -Wextra -Werror
+AR			:= ar
+AR_FLAGS 	:= -rcs
+OBJ_DIR		:= obj
+SRC_INCLUDE	:= get_next_line.h
+SRC			:= get_next_line.c		\
+			   get_next_line_utils.c
+INCLUDES	:= -I$(SRC_INCLUDE)
+OBJ			:= $(patsubst %.c, $(OBJ_DIR)/%.o, $(SRC))
+RM			:= rm -f
+
+all : $(NAME)
+
+$(NAME) : $(OBJ)
+	$(AR) $(AR_FLAGS) $@ $^
+
+$(OBJ_DIR)/%.o : %.c
+	mkdir -p $(OBJ_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDES) -c -o $@ $<
+
+clean :
+	$(RM) $(OBJ)
+
+fclean : clean
+	$(RM) $(NAME)
+	$(RM) -r $(OBJ_DIR)
+
+re : fclean all
+
+.PHONY : all clean fclean re
+
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidos- <davidos-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 11:21:46 by davidos-          #+#    #+#             */
+/*   Updated: 2026/01/11 11:25:49 by davidos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef	GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
+
