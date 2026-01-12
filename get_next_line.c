0327978b7e2ea75dffce094afa4755db9d76e064
p1nh4 <davidos-@student.42porto.com>
2026-01-12 21:12:13 +0000

    feat: add funcao para ler files

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: davidos- <davidos-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 11:20:56 by davidos-          #+#    #+#             */
-/*   Updated: 2026/01/11 17:29:40 by davidos-         ###   ########.fr       */
+/*   Updated: 2026/01/12 21:10:39 by davidos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,43 @@
 
 char	*get_next_line(int fd)
 {
-	int			nb_read;	//number bytes read
-	char		*str;
-	static char	buffer[BUFFER_SIZE + 1];
+	int			nb_read; //number bytes read of file descriptor
+	static char	*buffer;
 
-	nb_read = read(fd, buffer, BUFFER_SIZE);
-	if (nb_read == -1)
-		return ;
-	buffer[nb_read] = '\0';
-	return (str);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return ((void *)0);
+	buffer = ft_read_file(fd, buffer);
+	if (!buffer)
+		return ((void *)0);
+	return (buffer);
+}
+
+
+char	*ft_read_file(int fd, char *buffer)
+{
+	int		nb_read;	//number bytes read of file descriptor
+	char	*temp;
+	char	*ptr_buff;
+
+	nb_read = 1;
+	temp = malloc((BUFFER_SIZE + 1));
+	if (!temp)
+		return ((void *)0);
+	while (nb_read && !f_strchr(buffer, '\n'))
+	{
+		nb_read = read(fd, temp, BUFFER_SIZE);
+		if (nb_read < 0)
+			break ;
+		temp[nb_read] = '\0';
+		ptr_buff = buffer;
+		buffer = ft_strjoin(buffer, temp);
+		if (!buffer)
+			return ((void *)0);
+		free(ptr_buff);
+	}
+	free(temp);
+	if (nb_read < 0)
+		return ((void *)0);
+	else
+		return (buffer);
 }
